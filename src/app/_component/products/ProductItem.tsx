88d98688db3783e81299185a2ceb52200d6fc8a0
Mohamed Alaa <mohamed.alaa.elhefny@gmail.com>
2024-05-25 13:43:18 +0300

    fix ProductItem image error

--- src/app/_component/products/ProductItem.tsx
+++ src/app/_component/products/ProductItem.tsx
@@ -10,7 +10,9 @@ function ProductItem({ product }: ProductItemProps) {
     return (
         <Link href={`/product-details/${product?.id}`}>
             <div className="rounded-lg border border-transparent hover:border-orange-400 hover:shadow-md hover:cursor-pointer transition ease-in-out duration-300">
-                <Image src={imageUrl} alt="product-banner" width={400} height={350} className="rounded-t-lg h-[170px] object-cover" />
+                {imageUrl && (
+                    <Image src={imageUrl} alt="product-banner" width={400} height={350} className="rounded-t-lg h-[170px] object-cover" />
+                )}
                 <div className="flex justify-between p-3 items-center bg-gray-50 dark:bg-gray-800 rounded-b-lg">
                     <div>
                         <h2 className="text-[12px] font-medium line-clamp-1">
