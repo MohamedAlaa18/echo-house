333eca3df339183d75f9945e97f8c1af50d273c2
Mohamed Alaa <mohamed.alaa.elhefny@gmail.com>
2024-05-25 20:43:28 +0300

    add meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import "./globals.css";
 import Header from "./_component/header/Header";
 import Footer from "./_component/Footer";
-import { ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { CartContext } from "./_context/cartContext";
 
 const inter = Roboto({ subsets: ["latin"], weight: "700" });
@@ -43,6 +43,24 @@ export default function RootLayout({
     <ClerkProvider>
       <CartContext.Provider value={{ cart, setCart }}>
         <html lang='en'>
+          <head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Echo House is an e-commerce platform for courses, built using Next.js and Tailwind CSS. Browse available courses, view detailed product pages, and manage course purchases with our shopping cart functionality." />
+            <meta name="keywords" content="e-commerce, courses, online learning, Next.js, Tailwind CSS" />
+            <meta name="author" content="Echo House" />
+            <meta property="og:title" content="Echo House" />
+            <meta property="og:description" content="An e-commerce platform for courses built using Next.js and Tailwind CSS. Discover, learn, and manage your course purchases." />
+            <meta property="og:image" content="/path/to/your/image.jpg" />
+            <meta property="og:url" content="https://yourwebsite.com" />
+            <meta property="og:type" content="website" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content="Echo House" />
+            <meta name="twitter:description" content="An e-commerce platform for courses built using Next.js and Tailwind CSS. Discover, learn, and manage your course purchases." />
+            <meta name="twitter:image" content="/path/to/your/image.jpg" />
+            <link rel="icon" href="/logo.ico" />
+            <title>Echo House</title>
+          </head>
           <body className={`${inter.className} ${theme}`}>
             <Header toggleTheme={toggleTheme} theme={theme} />
             {children}
